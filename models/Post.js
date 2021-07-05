d2f99d88d51e8f7c0a829f769d369851fdcc6125
michaelzadra27 <michaelzadra27@gmail.com>
2021-07-05 14:55:13 -0700

    created models

---
+++ models/Post.js
@@ -0,0 +1,30 @@
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model { }
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+    },
+    body: {
+      type: DataTypes.STRING,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Post',
+  }
+);
+
+module.exports = Project;
