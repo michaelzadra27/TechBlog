a3466f6224210bdaac1e7aa7c9359729e39b8d7c
michaelzadra27 <michaelzadra27@gmail.com>
2021-07-05 17:05:04 -0700

    added dashboard route

---
+++ controllers/dashBoardRoutes.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+const { Post, Comment, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+        // Get all projects and JOIN with user data
+        const postData = await Post.findAll({
+            where: {
+                user_id: req.session.user_id
+            }
+        })
+        // Serialize data so the template can read it
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        // Pass serialized data and session flag into template
+        res.render('myposts', {
+            layout: 'dashboard',
+            posts
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/new', withAuth, (req, res) => {
+    res.render('newPost', {
+        layout: 'dashboard'
+    });
+});
+
+router.get("/edit/:id", withAuth, (req, res) => {
+    try {
+
+        const postData = await Post.findByPk(req.params.id)
+
+
+        const post = postData.map((post) => post.get({ plain: true }));
+
+        res.render("edit", {
+            layout: "dashboard",
+            post
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,8 +8,7 @@ router.get('/', async (req, res) => {
     const postData = await Post.findAll({
       include: [
         {
-          model: User,
-          attributes: ['name'],
+          model: User
         },
       ],
     });
@@ -18,7 +17,7 @@ router.get('/', async (req, res) => {
     const posts = postData.map((post) => post.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
+    res.render('blogPosts', { 
       posts, 
       logged_in: req.session.logged_in 
     });
